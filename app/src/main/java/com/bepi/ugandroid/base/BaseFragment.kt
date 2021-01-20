8a2e09c703120cecea6e7aec046f89f7d4de20ba
jarvis4901 <jarvis4901@sina.com>
2021-01-20 11:41:19 +0800

    feat:取件页面布局，basefragment 清空定时器逻辑挪到onPause

--- app/src/main/java/com/bepi/ugandroid/base/BaseFragment.kt
+++ app/src/main/java/com/bepi/ugandroid/base/BaseFragment.kt
@@ -2,6 +2,7 @@ package com.bepi.ugandroid.base
 
 import android.os.Bundle
 import android.os.CountDownTimer
+import android.util.Log
 import android.view.View
 import android.widget.TextView
 import androidx.fragment.app.Fragment
@@ -13,7 +14,7 @@ import java.lang.ref.WeakReference
 
 open class BaseFragment(val route: Route) :
     Fragment() {
-    private val COUNT_DELAY: Long = 2 * 1000 //定义总时长 120秒
+    private val COUNT_DELAY: Long = 120 * 1000 //定义总时长 120秒
     var countDownTimer: CountDownTimer? = null
 
 
@@ -32,20 +33,10 @@ open class BaseFragment(val route: Route) :
      */
     fun initHeader(view: View) {
         titleText?.setText(route.title)
+        backBlock?.setOnClickListener(View.OnClickListener { view -> backToHome(view) })
         //页面倒计时
         countDownTimer = object : CountDownTimer(COUNT_DELAY, 1000) {
-            override fun onFinish() {
-                //倒计时结束返回首页
-                var action: Int? = null
-                when (route.name) {
-                    "pickup" -> action = R.id.action_pickupFragment_to_homeFragment
-                    "deliver" -> action = R.id.action_deliverFragment_to_homeFragment
-                    "storage" -> action = R.id.action_storageFragment_to_homeFragment
-                    else -> action = R.id.action_sendFragment_to_homeFragment
-                }
-                Navigation.findNavController(view).navigate(action)
-            }
-
+            override fun onFinish() = backToHome(view)//结束倒计时返回首页
             override fun onTick(millisUntilFinished: Long) {
                 val second = (millisUntilFinished / 1000)
                 textCountDown?.setText("${second}秒")
@@ -53,8 +44,20 @@ open class BaseFragment(val route: Route) :
         }.start()
     }
 
-    override fun onDestroy() {
-        super.onDestroy()
+    fun backToHome(view: View) {
+        var action: Int? = null
+        when (route.name) {
+            "pickup" -> action = R.id.action_pickupFragment_to_homeFragment
+            "deliver" -> action = R.id.action_deliverFragment_to_homeFragment
+            "storage" -> action = R.id.action_storageFragment_to_homeFragment
+            else -> action = R.id.action_sendFragment_to_homeFragment
+        }
+        Navigation.findNavController(view).navigate(action)
+    }
+
+    override fun onPause() {
+        Log.i("baseFragment", "onPause")
+        super.onPause();
         countDownTimer?.cancel() //销毁定时器
         countDownTimer = null
     }
